a54154875b81b854b2cc9bd4360128016a2078b6
Rylan <rylanschaeffer@gmail.com>
2024-09-12 10:42:43 -0400

    Checking that VLLM will stop on EOS token.

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -13,7 +13,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "True"
 # This is needed for deterministic to work.
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
-from datasets import concatenate_datasets, load_dataset, DatasetDict
+from datasets import Dataset, DatasetDict
 import gc
 import numpy as np
 import pandas as pd
@@ -34,7 +34,7 @@ def sample_dataset_from_model(
     num_samples_per_sampling_call: int = 64,
 ):
     # Create dataset name.
-    dataset_name = f"{model_name_or_path.split('/')[1]}_temp={temperature}_max_seq_length={max_seq_length}"
+    dataset_name = f"{model_name_or_path.split('/')[1]}_temp{temperature}_max_seq_len{max_seq_length}"
     print(f"Dataset name: {dataset_name}")
 
     # Create empty prompts for sampling.
@@ -57,9 +57,11 @@ def sample_dataset_from_model(
 
         policy_model_sampling_params = SamplingParams(
             n=num_samples_per_sampling_call,
-            temperature=temperature,
             max_tokens=max_seq_length,
+            ignore_eos=False,
             seed=batch_generation_idx,
+            stop_token_ids=model.get_tokenizer().eos_token_id,
+            temperature=temperature,
         )
 
         requests_outputs: List[RequestOutput] = model.generate(
@@ -79,7 +81,7 @@ def sample_dataset_from_model(
             if "assistant:" not in sample:
                 continue
 
-            raw_prompt, raw_response = sample.split("assistant:")
+            raw_prompt, raw_response = sample.split("assistant: ", maxsplit=1)
             assert raw_prompt.startswith("user: ")
             dataset_prompts.append(raw_prompt.lstrip("user: ").strip())
             dataset_responses.append(raw_response.strip())
@@ -100,11 +102,19 @@ def sample_dataset_from_model(
     print("Sampled outputs from policy model.")
 
     # Create dataset.
-
-    dataset = dataset.map(compute_gold_reward_model_preferences, batched=False)
+    dataset = Dataset.from_dict(
+        {
+            "prompt": dataset_prompts,
+            "response": dataset_responses,
+        }
+    )
+    dataset_dict = DatasetDict({"train": dataset})
 
     # Push the dataset to HuggingFace.
-    commit_info = dataset.push_to_hub(repo_id=dataset_name)
+    print("Pushing the dataset to HuggingFace...")
+    commit_info = dataset_dict.push_to_hub(repo_id=dataset_name)
+    print(commit_info)
+    print("Pushed the dataset to HuggingFace!")
 
 
 if __name__ == "__main__":
