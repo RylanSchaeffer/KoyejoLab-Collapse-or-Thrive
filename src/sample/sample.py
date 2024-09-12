d1a7e2e92991d8a809f657af32880cb8b643fb4b
Rylan <rylanschaeffer@gmail.com>
2024-09-12 11:02:09 -0400

    Launching dataset sampling

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -13,7 +13,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "True"
 # This is needed for deterministic to work.
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
-from datasets import Dataset, DatasetDict
+from datasets import Dataset, DatasetDict, load_dataset
 import gc
 import numpy as np
 import pandas as pd
@@ -75,7 +75,7 @@ def sample_dataset_from_model(
         ]
 
         for sample in batch_samples:
-            # Only keep outputs that have both a user prompt and assistant response.
+            # Only keep outputs that have both a user prompt and an assistant response.
             if not sample.startswith("user: "):
                 continue
             if "assistant:" not in sample:
@@ -88,21 +88,11 @@ def sample_dataset_from_model(
             dataset_responses.append(processes_response)
 
         batch_generation_idx += 1
+        print(f"Sampled {len(dataset_prompts)} / {total_num_samples}.")
 
-    print("Sampled outputs from policy model.")
+    print("Sampled outputs from model.")
 
-    # Freeing up VLLM memory is harder than I thought!
-    # See: https://github.com/vllm-project/vllm/issues/1908
-    # Hit it with everything recommended!
-    destroy_model_parallel()
-    del model.llm_engine.model_executor.driver_worker
-    del model
-    gc.collect()
-    torch.cuda.empty_cache()
-    time.sleep(7)
-    print("Sampled outputs from policy model.")
-
-    # Create dataset.
+    # Create the dataset.
     dataset = Dataset.from_dict(
         {
             "prompt": dataset_prompts,
@@ -117,19 +107,34 @@ def sample_dataset_from_model(
     print(commit_info)
     print("Pushed the dataset to HuggingFace!")
 
+    # Freeing up VLLM memory is harder than I thought!
+    # See: https://github.com/vllm-project/vllm/issues/1908
+    # Hit it with everything recommended!
+    destroy_model_parallel()
+    del model.llm_engine.model_executor.driver_worker
+    del model
+    gc.collect()
+    torch.cuda.empty_cache()
+    time.sleep(7)
+    print("Sampled outputs from policy model.")
+
 
 if __name__ == "__main__":
     if "CUDA_VISIBLE_DEVICES" not in os.environ:
         os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(
             [str(i) for i in range(torch.cuda.device_count())]
         )
+
+    # dataset = load_dataset("nvidia/HelpSteer2")
     sample_dataset_from_model(
         model_name_or_path="RylanSchaeffer/collapse_gemma-2-2b_hs2_sftsd0_iter1",
         # max_seq_length=128,
         # max_seq_length=256,
         max_seq_length=512,
         # total_num_samples=10,
-        total_num_samples=64,
+        # total_num_samples=64,
         # total_num_samples=100,
+        total_num_samples=20324,  # The number of samples in the HelpSteer2 dataset.
     )
+
     print("Finished sample_outputs_from_policy_model.py!")
