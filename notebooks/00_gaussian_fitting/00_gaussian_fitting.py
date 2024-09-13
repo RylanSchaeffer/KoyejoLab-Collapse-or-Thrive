169abbfae4080b0dce804aad56189eb2868d0664
Rylan <rylanschaeffer@gmail.com>
2024-09-13 14:24:38 -0400

    Updating Gaussian fitting results per dimension

--- notebooks/00_gaussian_fitting/00_gaussian_fitting.py
+++ notebooks/00_gaussian_fitting/00_gaussian_fitting.py
@@ -91,83 +91,90 @@ for num_samples_per_iter in run_histories_df["Num. Samples per Iteration"].uniqu
         )
     # plt.show()
 
-plt.close()
-g = sns.relplot(
-    data=run_histories_df[run_histories_df["Data Dimension"] == 10],
-    x="Model-Fitting Iteration",
-    y="Squared Error of Fit Mean (Numerical)",
-    hue="Num. Samples per Iteration",
-    hue_norm=matplotlib.colors.LogNorm(),
-    col="Setting",
-    col_order=["Replace", "Accumulate"],
-    kind="line",
-    facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-    palette="mako_r",
-    legend="full",
-)
-g.set(yscale="log")
-g.set_axis_labels(
-    "Model-Fitting Iteration", r"$\lvert \lvert \hat{\mu}_n - \mu_0 \lvert \lvert_2^2$"
-)
-g.set_titles(
-    col_template="{col_name}",
-)
-sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-src.plot.save_plot_with_multiple_extensions(
-    plot_dir=results_dir,
-    plot_filename="squared_error_of_fit_mean_vs_model_fitting_iteration_by_noise_col=setting",
-)
-# plt.show()
 
-plt.close()
-g = sns.relplot(
-    data=run_histories_df[run_histories_df["Data Dimension"] == 10],
-    x="Model-Fitting Iteration",
-    y="Det of Fit Cov / Det of Init Cov (Numerical)",
-    hue="Num. Samples per Iteration",
-    hue_norm=matplotlib.colors.LogNorm(),
-    col="Setting",
-    col_order=["Replace", "Accumulate"],
-    kind="line",
-    facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-    palette="mako_r",
-    legend="full",
-)
-g.set_axis_labels("Model-Fitting Iteration", r"$det(\hat{\Sigma}_n) / det(\Sigma_0)$")
-g.set_titles(
-    col_template="{col_name}",
-)
-sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-src.plot.save_plot_with_multiple_extensions(
-    plot_dir=results_dir,
-    plot_filename="determinant_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting",
-)
-# plt.show()
+for (data_dim,), run_histories_data_dim_df in run_histories_df.groupby(
+    ["Data Dimension"]
+):
+    plt.close()
+    g = sns.relplot(
+        data=run_histories_data_dim_df,
+        x="Model-Fitting Iteration",
+        y="Squared Error of Fit Mean (Numerical)",
+        hue="Num. Samples per Iteration",
+        hue_norm=matplotlib.colors.LogNorm(),
+        col="Setting",
+        col_order=["Replace", "Accumulate"],
+        kind="line",
+        facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
+        palette="mako_r",
+        legend="full",
+    )
+    g.set(yscale="log")
+    g.set_axis_labels(
+        "Model-Fitting Iteration",
+        r"$\lvert \lvert \hat{\mu}_n - \mu_0 \lvert \lvert_2^2$",
+    )
+    g.set_titles(
+        col_template="{col_name}",
+    )
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    src.plot.save_plot_with_multiple_extensions(
+        plot_dir=results_dir,
+        plot_filename=f"squared_error_of_fit_mean_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
+    )
+    # plt.show()
 
-plt.close()
-g = sns.relplot(
-    data=run_histories_df[run_histories_df["Data Dimension"] == 10],
-    x="Model-Fitting Iteration",
-    y="Trace of Fit Cov / Trace of Init Cov (Numerical)",
-    hue="Num. Samples per Iteration",
-    hue_norm=matplotlib.colors.LogNorm(),
-    col="Setting",
-    col_order=["Replace", "Accumulate"],
-    kind="line",
-    facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-    palette="mako_r",
-    legend="full",
-)
-g.set_axis_labels("Model-Fitting Iteration", r"$Tr(\hat{\Sigma}_n) / Tr(\Sigma_0)$")
-g.set_titles(
-    col_template="{col_name}",
-)
-sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-src.plot.save_plot_with_multiple_extensions(
-    plot_dir=results_dir,
-    plot_filename="trace_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting",
-)
-# plt.show()
+    plt.close()
+    g = sns.relplot(
+        data=run_histories_data_dim_df,
+        x="Model-Fitting Iteration",
+        y="Det of Fit Cov / Det of Init Cov (Numerical)",
+        hue="Num. Samples per Iteration",
+        hue_norm=matplotlib.colors.LogNorm(),
+        col="Setting",
+        col_order=["Replace", "Accumulate"],
+        kind="line",
+        facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
+        palette="mako_r",
+        legend="full",
+    )
+    g.set_axis_labels(
+        "Model-Fitting Iteration", r"$det(\hat{\Sigma}_n) / det(\Sigma_0)$"
+    )
+    g.set_titles(
+        col_template="{col_name}",
+    )
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    src.plot.save_plot_with_multiple_extensions(
+        plot_dir=results_dir,
+        plot_filename=f"determinant_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
+    )
+    # plt.show()
+
+    plt.close()
+    g = sns.relplot(
+        data=run_histories_data_dim_df,
+        x="Model-Fitting Iteration",
+        y="Trace of Fit Cov / Trace of Init Cov (Numerical)",
+        hue="Num. Samples per Iteration",
+        hue_norm=matplotlib.colors.LogNorm(),
+        col="Setting",
+        col_order=["Replace", "Accumulate"],
+        kind="line",
+        facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
+        palette="mako_r",
+        legend="full",
+    )
+    g.set_axis_labels("Model-Fitting Iteration", r"$Tr(\hat{\Sigma}_n) / Tr(\Sigma_0)$")
+    g.set_titles(
+        col_template="{col_name}",
+    )
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    src.plot.save_plot_with_multiple_extensions(
+        plot_dir=results_dir,
+        plot_filename=f"trace_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
+    )
+    # plt.show()
 
 
 print("Finished running 00_gaussian_fitting.py")
