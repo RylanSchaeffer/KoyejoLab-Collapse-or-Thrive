c78e0017756d959952db00383d3c3b5b109069cf
Rylan <rylanschaeffer@gmail.com>
2024-09-14 11:54:17 -0400

    Pushing pair programmin session with Joshua.

--- notebooks/01_sft_language_model/01_sft_language_model.py
+++ notebooks/01_sft_language_model/01_sft_language_model.py
@@ -1,4 +1,3 @@
-import ast
 import matplotlib.pyplot as plt
 import matplotlib.transforms
 import numpy as np
@@ -20,19 +19,56 @@ data_dir, results_dir = src.analyze.setup_notebook_dir(
     refresh=False,
 )
 
-
-sweep_ids = [
+wandb_username = "rylan"
+wandb_sweep_ids = [
     "jlp5fu6u",  # HelpSteer2   Gemma2-2B   Data=Original   Iteration1
     "q8iad34m",  # HelpSteer2   Gemma2-2B   Data=Replace    Iteration2
 ]
 
-run_histories_df: pd.DataFrame = src.analyze.download_wandb_project_runs_histories(
-    wandb_project_path="rerevisiting-model-collapse-fit-gaussians",
+runs_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_configs(
+    wandb_project_path="rerevisiting-model-collapse-sft",
     data_dir=data_dir,
-    sweep_ids=sweep_ids,
+    sweep_ids=wandb_sweep_ids,
     refresh=refresh,
-    wandb_username=wandb.api.default_entity,
+    wandb_username=wandb_username,
+)
+
+# After this, we now have a column called "dataset" in runs_configs_df.
+runs_configs_df = src.analyze.extract_key_value_from_df_col(
+    df=runs_configs_df,
+    col_name="data_config",
+    key_in_dict="dataset",
+    new_col_name="dataset",
 )
 
 
+runs_histories_df: pd.DataFrame = src.analyze.download_wandb_project_runs_histories(
+    wandb_project_path="rerevisiting-model-collapse-sft",
+    data_dir=data_dir,
+    sweep_ids=wandb_sweep_ids,
+    refresh=refresh,
+    wandb_username=wandb_username,
+)
+
+extended_run_histories_df = runs_histories_df.merge(
+    runs_configs_df[["run_id", "dataset"]], left_on="run_id", right_on="run_id"
+)
+
+# This is a basic demonstration.
+plt.close()
+g = sns.relplot(
+    data=extended_run_histories_df,
+    kind="line",
+    x="train/epoch",
+    y="eval/loss",
+    col="dataset",
+    hue="dataset",
+)
+g.set_axis_labels("Epoch", "Loss")
+g.set_titles("{col_name}")
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="sft_language_model_loss_vs_epoch_by_dataset",
+)
+plt.show()
 print("Finished running 01_sft_language_model.py")
