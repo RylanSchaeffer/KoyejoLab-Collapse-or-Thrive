ab0a87d79288bfdd2472b2a979d5f9186f60df4f
Joshua Kazdan <jkazdan@skampere1.stanford.edu>
2024-09-16 12:49:15 -0700

    update the plotting code

--- notebooks/01_sft_language_model/01_sft_language_model.py
+++ notebooks/01_sft_language_model/01_sft_language_model.py
@@ -12,21 +12,23 @@ import src.plot
 
 
 refresh = False
-# refresh = True
+#refresh = True
 
 data_dir, results_dir = src.analyze.setup_notebook_dir(
     notebook_dir=os.path.dirname(os.path.abspath(__file__)),
     refresh=False,
 )
 
-wandb_username = "rylan"
+wandb_username = "jkazdan"
 wandb_sweep_ids = [
-    "jlp5fu6u",  # HelpSteer2   Gemma2-2B   Data=Original   Iteration1
-    "q8iad34m",  # HelpSteer2   Gemma2-2B   Data=Replace    Iteration2
+    "q3vd9gyn",  # HelpSteer2   Gemma2-2B   Data=Original   Iteration1
+    "2cvqmk2v",  # HelpSteer2   Gemma2-2B   Data=Replace    Iteration2
+    "wtr77bli",
+    "6s09ojgi",
 ]
 
 runs_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_configs(
-    wandb_project_path="rerevisiting-model-collapse-sft",
+    wandb_project_path="ft_collapse",
     data_dir=data_dir,
     sweep_ids=wandb_sweep_ids,
     refresh=refresh,
@@ -42,8 +44,11 @@ runs_configs_df = src.analyze.extract_key_value_from_df_col(
 )
 
 
+
+
+
 runs_histories_df: pd.DataFrame = src.analyze.download_wandb_project_runs_histories(
-    wandb_project_path="rerevisiting-model-collapse-sft",
+    wandb_project_path="ft_collapse",
     data_dir=data_dir,
     sweep_ids=wandb_sweep_ids,
     refresh=refresh,
@@ -54,6 +59,9 @@ extended_run_histories_df = runs_histories_df.merge(
     runs_configs_df[["run_id", "dataset"]], left_on="run_id", right_on="run_id"
 )
 
+# for i in range(len(wandb_sweep_ids)):
+#     extended_run_histories_df[extended_run_histories_df['wandb_sweep_ids'].eq(wandb_sweep_ids[i])]['model_fitting_iteration'] = i+1
+
 # This is a basic demonstration.
 plt.close()
 g = sns.relplot(
@@ -61,7 +69,7 @@ g = sns.relplot(
     kind="line",
     x="train/epoch",
     y="eval/loss",
-    col="dataset",
+    #col="dataset",
     hue="dataset",
 )
 g.set_axis_labels("Epoch", "Eval Loss on Real Data")
@@ -73,3 +81,21 @@ src.plot.save_plot_with_multiple_extensions(
 )
 plt.show()
 print("Finished running 01_sft_language_model.py")
+
+
+print(extended_run_histories_df.keys())
+plt.close()
+g = sns.relplot(
+    data=extended_run_histories_df,
+    x="model_fitting_iteration",
+    y="eval/loss",
+    hue="_step",
+)
+g.set_axis_labels("Fitting Iteration", "Eval Loss on Real Data")
+g.set_titles("{col_name}")
+sns.move_legend(g, "upper left", bbox_to_anchor=(1.0, 1.0))
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="sft_language_model_eval_vs_fitting_iteration",
+)
+plt.show() 
