6cd9f0ae19ec7beac251728aefabb57c79be4fa2
Rylan <rylanschaeffer@gmail.com>
2024-09-25 15:15:05 -0400

    KDE notebook is ready to go!

--- notebooks/02_kde_fitting/02_kde_fitting.py
+++ notebooks/02_kde_fitting/02_kde_fitting.py
@@ -21,101 +21,129 @@ data_dir, results_dir = src.analyze.setup_notebook_dir(
 )
 
 
-sweep_ids = [
-    "u1g0lzn0",  # Moons KDE (~5k runs).
+wandb_sweep_ids = [
+    "ybhk4l3h",  # Moons KDE (~6k runs).
 ]
+
+runs_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_configs(
+    wandb_project_path="rerevisiting-model-collapse-fit-kdes",
+    data_dir=data_dir,
+    sweep_ids=wandb_sweep_ids,
+    refresh=refresh,
+    wandb_username="rylan",
+    finished_only=True,
+)
+
+keys_to_extract_from_cols = [
+    (
+        "data_config",
+        "dataset_name",
+        "Dataset",
+    ),
+]
+
+# Extract what we need from nested dictionaries.
+for key_to_extract in keys_to_extract_from_cols:
+    runs_configs_df = src.analyze.extract_key_value_from_df_col(
+        df=runs_configs_df,
+        col_name=key_to_extract[0],
+        key_in_dict=key_to_extract[1],
+        new_col_name=key_to_extract[2],
+    )
+
+
+# Rename columns to make them a little nicer.
+runs_configs_df = runs_configs_df.rename(
+    columns={
+        "kernel": "Kernel",
+        "kernel_bandwidth": r"Bandwidth $h$",
+        "num_samples_per_iteration": "Num. Samples per Iteration",
+        "setting": "Setting",
+    }
+)
 
 run_histories_df: pd.DataFrame = src.analyze.download_wandb_project_runs_histories(
     wandb_project_path="rerevisiting-model-collapse-fit-kdes",
     data_dir=data_dir,
-    sweep_ids=sweep_ids,
+    sweep_ids=wandb_sweep_ids,
     refresh=refresh,
     wandb_username=wandb.api.default_entity,
 )
 
+run_histories_df = run_histories_df.rename(
+    columns={"Mean Negative Log Prob (Test)": "Eval NLL on Real Data"}
+)
 
-# for (data_dim,), run_histories_data_dim_df in run_histories_df.groupby(
-#     ["Data Dimension"]
-# ):
-#     plt.close()
-#     g = sns.relplot(
-#         data=run_histories_data_dim_df,
-#         x="Model-Fitting Iteration",
-#         y="Squared Error of Fit Mean (Numerical)",
-#         hue="Num. Samples per Iteration",
-#         hue_norm=matplotlib.colors.LogNorm(),
-#         col="Setting",
-#         col_order=["Replace", "Accumulate"],
-#         kind="line",
-#         facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-#         palette="mako_r",
-#         legend="full",
-#     )
-#     g.set(yscale="log")
-#     g.set_axis_labels(
-#         "Model-Fitting Iteration",
-#         r"$\lvert \lvert \hat{\mu}_n - \mu_0 \lvert \lvert_2^2$",
-#     )
-#     g.set_titles(
-#         col_template="{col_name}",
-#     )
-#     sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-#     src.plot.save_plot_with_multiple_extensions(
-#         plot_dir=results_dir,
-#         plot_filename=f"squared_error_of_fit_mean_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
-#     )
-#     # plt.show()
-#
-#     plt.close()
-#     g = sns.relplot(
-#         data=run_histories_data_dim_df,
-#         x="Model-Fitting Iteration",
-#         y="Det of Fit Cov / Det of Init Cov (Numerical)",
-#         hue="Num. Samples per Iteration",
-#         hue_norm=matplotlib.colors.LogNorm(),
-#         col="Setting",
-#         col_order=["Replace", "Accumulate"],
-#         kind="line",
-#         facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-#         palette="mako_r",
-#         legend="full",
-#     )
-#     g.set_axis_labels(
-#         "Model-Fitting Iteration", r"$det(\hat{\Sigma}_n) / det(\Sigma_0)$"
-#     )
-#     g.set_titles(
-#         col_template="{col_name}",
-#     )
-#     sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-#     src.plot.save_plot_with_multiple_extensions(
-#         plot_dir=results_dir,
-#         plot_filename=f"determinant_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
-#     )
-#     # plt.show()
-#
-#     plt.close()
-#     g = sns.relplot(
-#         data=run_histories_data_dim_df,
-#         x="Model-Fitting Iteration",
-#         y="Trace of Fit Cov / Trace of Init Cov (Numerical)",
-#         hue="Num. Samples per Iteration",
-#         hue_norm=matplotlib.colors.LogNorm(),
-#         col="Setting",
-#         col_order=["Replace", "Accumulate"],
-#         kind="line",
-#         facet_kws={"sharey": True, "sharex": True, "margin_titles": True},
-#         palette="mako_r",
-#         legend="full",
-#     )
-#     g.set_axis_labels("Model-Fitting Iteration", r"$Tr(\hat{\Sigma}_n) / Tr(\Sigma_0)$")
-#     g.set_titles(
-#         col_template="{col_name}",
-#     )
-#     sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-#     src.plot.save_plot_with_multiple_extensions(
-#         plot_dir=results_dir,
-#         plot_filename=f"trace_of_fit_cov_vs_model_fitting_iteration_by_noise_col=setting_dim={data_dim}",
-#     )
-#     # plt.show()
+extended_run_histories_df = run_histories_df.merge(
+    runs_configs_df[
+        [
+            "run_id",
+            "Dataset",
+            "Kernel",
+            r"Bandwidth $h$",
+            "Num. Samples per Iteration",
+            "Setting",
+        ]
+    ],
+    on="run_id",
+    how="inner",
+)
+
+plt.close()
+g = sns.relplot(
+    data=extended_run_histories_df,
+    kind="line",
+    x="Model-Fitting Iteration",
+    y="Eval NLL on Real Data",
+    col="Setting",
+    col_order=["Replace", "Accumulate"],
+    row="Dataset",
+    hue="Num. Samples per Iteration",
+    hue_norm=matplotlib.colors.LogNorm(),
+    palette="mako_r",
+    legend="full",
+    facet_kws={"sharex": True, "sharey": "row", "margin_titles": True},
+)
+g.set(yscale="log")
+g.set_titles(
+    col_template="{col_name}",
+    row_template="Dataset: {row_name}",
+)
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename=f"neg_log_likelihood_vs_model_fitting_iteration_by_noise_col=setting_row=dataset",
+)
+# plt.show()
+
+for bandwidth, bandwidth_group_df in extended_run_histories_df.groupby(
+    r"Bandwidth $h$"
+):
+    plt.close()
+    g = sns.relplot(
+        data=bandwidth_group_df,
+        kind="line",
+        x="Model-Fitting Iteration",
+        y="Eval NLL on Real Data",
+        col="Setting",
+        col_order=["Replace", "Accumulate"],
+        row="Dataset",
+        hue="Num. Samples per Iteration",
+        hue_norm=matplotlib.colors.LogNorm(),
+        palette="mako_r",
+        legend="full",
+        facet_kws={"sharex": True, "sharey": "row", "margin_titles": True},
+    )
+    g.set(yscale="log")
+    g.set_titles(
+        col_template="{col_name}",
+        row_template="Dataset: {row_name}",
+    )
+    sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+    src.plot.save_plot_with_multiple_extensions(
+        plot_dir=results_dir,
+        plot_filename=f"neg_log_likelihood_vs_model_fitting_iteration_by_noise_col=setting_row=dataset_bandwidth={bandwidth}",
+    )
+    # plt.show()
+
 print("Finished running 02_kde_fitting.py")
-raise NotImplementedError
